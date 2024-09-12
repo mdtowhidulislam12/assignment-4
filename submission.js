e6c283826f1cb2f3525d83db5879ce97064a5745
MD TOWHIDUL ISLAM <mohammadtowhidulislam999@gmail.com>
2024-09-12 20:08:47 +0600

    second commit

--- submission.js
+++ submission.js
@@ -1,4 +1,3 @@
-//problem-1
 function calculateTax(income, expenses) {
     if (income < 0 || income < expenses || expenses < 0){
     return "Invalid Input";
@@ -8,10 +7,6 @@ function calculateTax(income, expenses) {
 
     return tax;
 };
-
-// const income = 34000;
-// const expenses = 1753;
-// console.log(calculateTax(income ,expenses));
 
 
 
@@ -23,8 +18,8 @@ function sendNotification(email) {
    const massage = `${username} sent you an email from ${domain_name}`;
    return massage;
 };
-// const email = "nadim.naem5@outlook.com";
-// console.log(sendNotification(email));
+
+
 
 function checkDigitsInName(name) {
     if(typeof name !== 'string'){
@@ -39,9 +34,6 @@ function checkDigitsInName(name) {
     }
     return false;
 };
-
-//console.log(checkDigitsInName('emu'));
-
 
 
 
@@ -64,36 +56,22 @@ function calculateFinalScore(obj) {
     return false;
 };
 
-const object = {
-      
-        name: "Rajib", 
-        testScore: 25,  
-        schoolGrade: 35, 
-        isFFamily : true
-    };
-    //console.log(calculateFinalScore(object));
 
-    //problem-5
 
 function  waitingTime(waitingTimes  , serialNumber) {
 
-        if(!Array.isArray(waitingTimes) || typeof serialNumber !== 'number'){
-            return "Invalid Input"
-        }
-       
-       let times = 0;
-       for ( i = 0; i < waitingTimes.length; i++){
-        times = waitingTimes[i] + times;
-       }
-       
-       let avg = times / waitingTimes.length;
-        let avgF = Math.round(avg);
-    let calc = avgF * (serialNumber - (waitingTimes.length + 1));
-    return calc;
-       
+    if(!Array.isArray(waitingTimes) || typeof serialNumber !== 'number'){
+        return "Invalid Input"
+    }
+   
+   let times = 0;
+   for ( i = 0; i < waitingTimes.length; i++){
+    times = waitingTimes[i] + times;
+   }
+   
+   let avg = times / waitingTimes.length;
+    let avgF = Math.round(avg);
+let calc = avgF * (serialNumber - (waitingTimes.length + 1));
+return calc;
+   
 };
-// const array = [ 13,2 ];
-// const serialNumber = '6';
-//  console.log(waitingTime(array , serialNumber)); 
-
-
